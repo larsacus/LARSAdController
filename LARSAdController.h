e771f7874d60edd84545beb153a618f913d24819
Lars <ishka912@gmail.com>
2011-07-24 17:55:24 -0500

    Initial commit

---
+++ LARSAdController.h
@@ -0,0 +1,13 @@
+//
+//  LARSAdController.h
+//  RealLED
+//
+//  Created by Lars on 7/24/11.
+//  Copyright 2011 Drink & Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LARSAdController : NSObject
+
+@end
---
+++ LARSAdController.m
@@ -0,0 +1,23 @@
+//
+//  LARSAdController.m
+//  RealLED
+//
+//  Created by Lars on 7/24/11.
+//  Copyright 2011 Drink & Apple. All rights reserved.
+//
+
+#import "LARSAdController.h"
+
+@implementation LARSAdController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end

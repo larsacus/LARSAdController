03f48704be62938db5798ced630fa82d6969867d
Lars <ishka912@gmail.com>
2011-07-24 19:47:53 -0500

    + Added skeleton delegate methods for Google Ads and Apple iAds + Added ivars and accessors for privates

--- LARSAdController.m
+++ LARSAdController.m
@@ -18,14 +18,96 @@
 
 @implementation LARSAdController
 
-- (id)init
-{
-    self = [super init];
-    if (self) {
-        // Initialization code here.
+@synthesize adMobBannerView             = _adMobBannerView;
+@synthesize iAdBannerView               = _iAdBannerView;
+@synthesize parentView                  = _parentView;
+
+static LARSAdController *sharedController = nil;
+
+#pragma mark -
+#pragma mark Class Methods
+
++ (LARSAdController *)sharedManager{
+    if (sharedController == nil) {
+        sharedController = [[super allocWithZone:NULL] init];
     }
-    
+    return sharedController;
+}
+
++ (id)allocWithZone:(NSZone *)zone{
+    return [[self sharedManager] retain];
+}
+
+#pragma mark -
+#pragma mark Singleton Implementation
+
+- (id)copyWithZone:(NSZone *)zone{
     return self;
+}
+
+- (id)retain{
+    return self;
+}
+
+- (NSUInteger)retainCount{
+    return NSUIntegerMax;
+}
+
+- (id)autorelease{
+    return self;
+}
+
+#pragma mark -
+#pragma mark Common Methods
+
+#pragma mark -
+#pragma mark iAd Delegate Methods
+- (void)bannerViewDidLoadAd:(ADBannerView *)banner{
+    
+}
+
+- (BOOL)bannerViewActionShouldBegin:(ADBannerView *)banner willLeaveApplication:(BOOL)willLeave{
+    return YES;
+}
+
+- (void)bannerViewActionDidFinish:(ADBannerView *)banner{
+    
+}
+
+- (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error{
+    
+}
+
+#pragma mark -
+#pragma mark AdMob/Google Methods
+- (void)createAdMobAds{
+    
+}
+
+- (void)destroyAdMobAds{
+    
+}
+
+#pragma mark -
+#pragma mark AdMob/Google Delegate Methods
+- (void)adViewDidReceiveAd:(GADBannerView *)bannerView{
+    
+}
+
+- (void)adView:(GADBannerView *)bannerView didFailToReceiveAdWithError:(GADRequestError *)error{
+    
+}
+
+- (void)adViewWillPresentScreen:(GADBannerView *)bannerView{
+    
+}
+
+- (void)adViewDidDismissScreen:(GADBannerView *)bannerView{
+    
+}
+
+- (void)adViewWillLeaveApplication:(GADBannerView *)bannerView{
+    
 }
 
 @end

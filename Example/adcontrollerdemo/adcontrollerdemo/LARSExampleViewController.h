868c9857c9784b4a5b0e820ea8275e14586c9f09
Matt DiDomenico <Matt@Matts-MacBook-Pro.local>
2013-08-23 02:35:42 -0400

    Updated sample project to include support for pushing content up/down when an add appears/disappears. In this example, a button is placed at the bottom of the screen when an ad shows or hides itself. It also works when an ad of different size appears, such as when an Admob ad is shown after an iAd ad is shown.

--- Example/adcontrollerdemo/adcontrollerdemo/LARSExampleViewController.h
+++ Example/adcontrollerdemo/adcontrollerdemo/LARSExampleViewController.h
@@ -12,5 +12,8 @@
 @interface LARSExampleViewController : TOLAdViewController
 
 @property (nonatomic, retain) UILabel *adVisibleLabel;
+@property (nonatomic, retain) UIButton *bottomTestButton;
+@property (nonatomic) BOOL bannerIsVisible;                 // shows if an ad is visible on this view or not
+@property (nonatomic) float currentPushAmount;              // the amount that the test button (or anything else) is being pushed up at the moment
 
 @end
--- Example/adcontrollerdemo/adcontrollerdemo/LARSExampleViewController.m
+++ Example/adcontrollerdemo/adcontrollerdemo/LARSExampleViewController.m
@@ -8,6 +8,7 @@
 
 #import "LARSExampleViewController.h"
 #import "LARSAdController.h"
+#import "TOLAdAdapter.h"
 
 @interface LARSExampleViewController ()
 
@@ -25,6 +26,20 @@
     
     [view addSubview:self.adVisibleLabel];
     
+    
+    float buttonHeight = 50.0f;
+    float buttonWidth = view.frame.size.width - 20.0f;
+    
+    // create and configure the test button at the bottom of the screen
+    self.bottomTestButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    self.bottomTestButton.center = CGPointMake(0.0, 0.0);
+    self.bottomTestButton.frame = CGRectMake(10.0f, view.frame.size.height - (buttonHeight + 30.0f), buttonWidth, buttonHeight);
+    [self.bottomTestButton setTitle:@"this button should move up and down when an ad is shown or hidden" forState:UIControlStateNormal];
+    self.bottomTestButton.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    self.bottomTestButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    
+    [view addSubview:self.bottomTestButton];
+    
     self.view = view;
 }
 
@@ -38,18 +53,59 @@
                                        forKeyPath:kLARSAdObserverKeyPathIsAdVisible
                                           options:NSKeyValueObservingOptionNew
                                           context:nil];
+    
+    self.bannerIsVisible = NO;
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context{
     if ([keyPath isEqualToString:kLARSAdObserverKeyPathIsAdVisible]) {
         NSLog(@"Observing keypath \"%@\"", keyPath);
         
+        
         BOOL anyAdsVisible = [change[NSKeyValueChangeNewKey] boolValue];
+        
+        // get a reference to the banner that is currently visible
+        UIView *bannerView;
+        
+        NSArray *instances = [[[LARSAdController sharedManager] adapterInstances] allValues];
+        for (id <TOLAdAdapter> adapterInstance in instances) {
+            if (adapterInstance.adVisible) {
+                bannerView = adapterInstance.bannerView;
+            }
+        }
+        
         if (anyAdsVisible) {
             self.adVisibleLabel.text = @"Ad is visible";
-        }
-        else{
+            
+            // an add either appeared for the first time or replaced an existing one.
+            // either way, figure out how high to move the button, and move it
+            float height = bannerView.frame.size.height;
+            float padding = 0.0f;
+            self.currentPushAmount = height + padding;
+            
+            self.bannerIsVisible = YES;
+            [UIView animateWithDuration:0.25f animations:^(void) {
+                CGRect screenRect = [[UIScreen mainScreen] bounds];
+                
+                [self.bottomTestButton setFrame:CGRectMake(self.bottomTestButton.frame.origin.x,
+                                                           screenRect.size.height - (self.currentPushAmount + self.bottomTestButton.frame.size.height + 30.0f),
+                                                           self.bottomTestButton.frame.size.width,
+                                                           self.bottomTestButton.frame.size.height)];
+            }];
+            
+        } else {
             self.adVisibleLabel.text = @"Ad not visible";
+            
+            // no ads are visible, but if the banner is still showing, so move it back down
+            if (self.bannerIsVisible) {
+                self.bannerIsVisible = NO;
+                [UIView animateWithDuration:0.25f animations:^(void) {
+                    [self.bottomTestButton setFrame:CGRectMake(self.bottomTestButton.frame.origin.x,
+                                                               self.bottomTestButton.frame.origin.y + self.currentPushAmount,
+                                                               self.bottomTestButton.frame.size.width,
+                                                               self.bottomTestButton.frame.size.height)];
+                }];
+            }
         }
     }
 }
@@ -58,14 +114,14 @@
     [[LARSAdController sharedManager] removeObserver:self forKeyPath:kLARSAdObserverKeyPathIsAdVisible];
 }
 
-/* If this view controller did not inherit from TOLAdViewController, then you would need to uncomment the below method and implement adding the ad container to the view yourself. You will also need to uncomment line 10 to import the class. 
+/* If this view controller did not inherit from TOLAdViewController, then you would need to uncomment the below method and implement adding the ad container to the view yourself. You will also need to uncomment line 10 to import the class.
  */
 /*
-- (void)viewWillAppear:(BOOL)animated{
-    [super viewWillAppear:animated];
-    
-    [[LARSAdController sharedManager] addAdContainerToViewInViewController:self];
-}
+ - (void)viewWillAppear:(BOOL)animated{
+ [super viewWillAppear:animated];
+ 
+ [[LARSAdController sharedManager] addAdContainerToViewInViewController:self];
+ }
  */
 
 //Deprecated
